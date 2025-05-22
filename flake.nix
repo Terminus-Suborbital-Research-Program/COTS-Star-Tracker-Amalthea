0b1037f211051c08c60208e02dd4ded2328dd647
Lucas Thelen <thelenlucas028@gmail.com>
2025-05-21 19:09:19 -0500

    Initial flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    vimbax.url = "github:Terminus-Suborbital-Research-Program/vimba-x";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, vimbax }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        # Vimba libs
+        vimbax-lib = vimbax.packages.${system}.vimbax;
+
+        # Vmbpy lib
+        vmbpy = vimbax.packages.${system}.vmbpy;
+
+        # Additional libraries
+        additionalLibraries = with pkgs.python312Packages; [ aenum numpy ];
+
+        allPythonLibs = [ vmbpy ] ++ additionalLibraries;
+      in {
+        #
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ (pkgs.python311.withPackages (ps: allPythonLibs)) ];
+        };
+      });
+}
